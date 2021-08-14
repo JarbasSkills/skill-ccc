b864a900db1b9df97f6aea751f105116a34f6db3
jarbasal <jarbasai@mailfence.com>
2021-08-14 14:02:16 +0100

    .

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 from mycroft.skills.core import intent_file_handler
 from pyvod import Collection, Media
 from os.path import join, dirname, basename
-from ovos_workshop.frameworks.playback import CPSMatchType, CPSPlayback, \
-    CPSMatchConfidence
+from ovos_workshop.frameworks.playback import CommonPlayMediaType, CommonPlayPlaybackType, \
+    CommonPlayMatchConfidence
 from ovos_workshop.skills.video_collection import VideoCollectionSkill
 
 
@@ -10,10 +10,10 @@ class CultCinemaClassicsSkill(VideoCollectionSkill):
 
     def __init__(self):
         super().__init__("CultCinemaClassics")
-        self.supported_media = [CPSMatchType.GENERIC,
-                                CPSMatchType.MOVIE,
-                                CPSMatchType.TRAILER,
-                                CPSMatchType.VIDEO]
+        self.supported_media = [CommonPlayMediaType.GENERIC,
+                                CommonPlayMediaType.MOVIE,
+                                CommonPlayMediaType.TRAILER,
+                                CommonPlayMediaType.VIDEO]
         path = join(dirname(__file__), "res", "CultCinemaClassics.jsondb")
         logo = join(dirname(__file__), "res", "ccc_logo.png")
         # load video catalog
@@ -24,8 +24,8 @@ class CultCinemaClassicsSkill(VideoCollectionSkill):
         self.skill_logo = join(dirname(__file__), "ui", "ccc_icon.jpg")
         self.skill_icon = join(dirname(__file__), "ui", "ccc_icon.jpg")
         self.default_bg = join(dirname(__file__), "ui", "ccc_logo.png")
-        self.playback_type = CPSPlayback.GUI
-        self.media_type = CPSMatchType.MOVIE
+        self.playback_type = CommonPlayPlaybackType.GUI
+        self.media_type = CommonPlayMediaType.MOVIE
 
     # voice interaction
     def get_intro_message(self):
@@ -38,11 +38,11 @@ class CultCinemaClassicsSkill(VideoCollectionSkill):
     # matching
     def match_media_type(self, phrase, media_type):
         score = 0
-        if self.voc_match(phrase, "video") or media_type == CPSMatchType.VIDEO:
+        if self.voc_match(phrase, "video") or media_type == CommonPlayMediaType.VIDEO:
             score += 5
         if self.voc_match(phrase, "classic"):
             score += 10
-        if self.voc_match(phrase, "movie") or media_type == CPSMatchType.MOVIE:
+        if self.voc_match(phrase, "movie") or media_type == CommonPlayMediaType.MOVIE:
             score += 10
         if self.voc_match(phrase, "ccc"):
             score += 50
